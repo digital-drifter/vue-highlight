b88608454add5aef80afbd3614b9201e46d8abf3
Brian Lee <brian@vandepoelproductions.com>
2019-01-03 22:42:25 -0700

    import php and js support

--- src/main.ts
+++ src/main.ts
@@ -1,6 +1,13 @@
-import Vue, { PluginFunction, VueConstructor } from 'vue'
-import { highlightBlock } from 'highlight.js'
+import Vue, { VueConstructor } from 'vue'
+import hljs from 'highlight.js'
+import javascript from 'highlight.js/lib/languages/javascript'
+import php from 'highlight.js/lib/languages/php'
+import 'highlight.js/styles/tomorrow-night-eighties.css'
 import VueHighlight, { VueHighlightOptions } from '../types'
+
+hljs.fixMarkup('true')
+hljs.registerLanguage('javascript', javascript as any)
+hljs.registerLanguage('php', php as any)
 
 class Highlighter implements VueHighlight {
   options: VueHighlightOptions
@@ -9,16 +16,16 @@ class Highlighter implements VueHighlight {
     this.options = options
   }
 
-  blocks (selector: string = 'pre code'): void {
-    document.querySelectorAll(selector).forEach(highlightBlock)
-  }
-
   static install (vm: VueConstructor<Vue>, options?: any): never | void {
     const instance = new vm()
 
     Object.defineProperty(vm.prototype, '$highlight', {
       value: new Highlighter(Object.assign(options || {}, { $isServer: instance.$isServer }))
     })
+  }
+
+  blocks (selector: string = 'pre code'): void {
+    document.querySelectorAll(selector).forEach(hljs.highlightBlock)
   }
 }
 
